2c8a4dd802553c389fcbbc9dd9f00152684b059c
Andres Alejos <andres.alejos@gmail.com>
2025-01-09 09:11:31 -0500

    bump minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Merquery.MixProject do
   def project do
     [
       app: :merquery,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
